6c17ff947a79a41d9517e474d37e87783a39eef3
RehanShakir <rehanshakir2@gmail.com>
2021-03-19 10:32:23 +0500

    Final Commit

--- js/form.js
+++ js/form.js
@@ -6,16 +6,24 @@ function handleFormSubmit(e) {
   if ($("#name").val() <= 5) {
     $("#name").css({ border: "2px solid red" });
     $("#nameP").text("Name Must be at least 5 characters long");
-  } else if ($("#email").val() == 0) {
+  }
+  if ($("#email").val() == 0) {
     $("#email").css({ border: "2px solid red" });
     $("#emailP").text("Please enter email");
-  } else if ($("#password").val() < 8) {
+  }
+  if ($("#password").val() < 8) {
     $("#password").css({ border: "2px solid red" });
     $("#passwordP").text("Password Must be at least 8 characters long");
-  } else if ($("#password").val() != $("#confirmPassword").val()) {
-    $("#password").css({ border: "2px solid red" });
-    $("#confirmpasswordP").text("Password not Matched");
-  } else {
+  }
+  if ($("#password").val() != $("#confirmPassword").val() && $("#password")) {
+    $("#confirmPassword").css({ border: "2px solid red" });
+    $("#confirmPasswordP").text("Password not Matched");
+  }
+  if (
+    $("#password").val() == $("#confirmPassword").val() &&
+    $("#password").val() >= 8 &&
+    $("$name").val() >= 5
+  ) {
     $("#submitP").text("Form is being submitted");
   }
   e.preventDefault();
